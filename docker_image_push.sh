fe88e4c56d5852651f6add28bc875caa8c141eae
Gazanfar <97215070+sgazanfar@users.noreply.github.com>
2022-11-24 21:50:52 +0530

    Update docker_image_push.sh

--- docker_image_push.sh
+++ docker_image_push.sh
@@ -1,7 +1,6 @@
 #!  /bin/bash
 profile="default"
-region="us-west-2"
-buildTag="shellscript-demo:latest"
+region="us-east-2"
 
 echo "$1"
 echo "$2"
@@ -20,7 +19,7 @@ if [[ -n "$3" ]]; then
 fi
 
 
-aws ecr get-login-password --region "${region}" --profile "${profile}" | docker login --username AWS --password-stdin "120717539064.dkr.ecr.${region}.amazonaws.com"
-docker build -t "${buildTag}" .
-docker tag "${buildTag}" "120717539064.dkr.ecr.us-west-2.amazonaws.com/${buildTag}"
-docker push "120717539064.dkr.ecr.us-west-2.amazonaws.com/${buildTag}"
+aws ecr get-login-password --region "${region}" --profile "${profile}" | docker login --username AWS --password-stdin "074108680633.dkr.ecr.${region}.amazonaws.com"
+docker build -t "devproject" .
+docker tag "devproject:latest 074108680633.dkr.ecr.us-east-2.amazonaws.com/devproject:latest"
+docker push "074108680633.dkr.ecr.us-east-2.amazonaws.com/devproject:latest"
